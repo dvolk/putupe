b4825051e04b8d7f386aa962333e8da40589861b
Denis Volk <denis.volk@gmail.com>
2022-01-21 20:55:41 +0000

    don't add character if it's not in the presented line

--- putupe.py
+++ putupe.py
@@ -18,17 +18,19 @@ def go(filename, resume_line=0):
             print("> " + "".join(userline) + "_")
         with term.cbreak(), term.hidden_cursor():
             inp = term.inkey()
-            if inp.name == "KEY_BACKSPACE":
-                if userline:
-                    userline.pop()
-            elif inp.name == "KEY_ENTER":
-                if "".join(userline) == line:
-                    userline = list()
-                    line_num += 1
-            elif inp.name == "KEY_ESCAPE":
-                break
-            else:
-                userline.append(inp)
+
+        if inp.name == "KEY_BACKSPACE":
+            if userline:
+                userline.pop()
+        elif inp.name == "KEY_ENTER":
+            if "".join(userline) == line:
+                userline = list()
+                line_num += 1
+        elif inp.name == "KEY_ESCAPE":
+            break
+        elif inp in line:
+            userline.append(inp)
+
     print(term.clear)
     print(f"last line index: {line_num}")
 
